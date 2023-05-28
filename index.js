86a9c59220788591ad31c09f0e7aae373ee32bcd
mfrhn-1 <127017587+mfrhn-1@users.noreply.github.com>
2023-05-28 15:53:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ bot.on("message", async (ctx) => {
     const regex = /^https:\/\/(id\.|www\.)?scribd\.com\/(document|doc|presentation)\/(\d{8,9})\/.*/;
     const match = ctx.message.text.match(regex);
     if (match) {
-      const url = `https://www.scribd.com/embeds/${match[2]}/content/`;
+      const url = `https://id.scribd.com/embeds/${match[2]}/content/`;
       const options = {
         reply_markup: {
           inline_keyboard: [
